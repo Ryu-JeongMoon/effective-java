7add8040b88855b2abe1876e80df20632131dce2
jm <rjm9303@gmail.com>
2021-12-31 17:04:02 +0900

    Item 56, 57

---
+++ src/main/java/chap09/item57/Item57.java
@@ -0,0 +1,39 @@
+package chap09.item57;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * 지역변수의 범위를 최소화하라
+ */
+public class Item57 {
+
+    public static void main(String[] args) {
+        List<String> strings = new ArrayList<>(List.of("y", "a", "h", "o"));
+
+        // intellisense 바꾸라고 알려준다
+        Iterator<String> i = strings.iterator();
+        while (i.hasNext()) {
+            System.out.println(i.next());
+        }
+
+        for (String string : strings) {
+            System.out.println(string);
+        }
+    }
+}
+
+/*
+item15 와 취지가 비슷하다
+어떤 변수든 가능하면 최소 범위를 유지하라 가독성이 좋아지고, 오류 발생이 적어진다
+위 효과를 얻기 위해 사용할 수 있는 가장 강력하고 간단한 방법은 사용하기 바로 전에 선언하는 것
+
+자신이 코드를 짤 때는 모를 수 있다
+코드 작성에는 항상 남이 볼 때 어떤 모습일지 염두해두어야 한다
+잘 쓴 소설과 같이 보일지 vs 맘대로 끄적인 낙서 같이 보일지
+
+반복자를 직접 써야하는 경우가 아니라면 while 보다는 for 쓸 것
+while - 반복 범위 외부에서 iterator 로 변수 선언해줘야 하고 이 변수의 영향이 메서드 내부로 퍼져나갈 수 있다
+for - 반복 범위 내에서만 유효한 변수 선언되고 유효 범위가 반복문 내로 한정된다
+ */
---
+++ src/main/java/chap09/item58/Item58.java
@@ -0,0 +1,66 @@
+package chap09.item58;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * 전통적인 for 문보다는 for-each 문을 사용하라
+ */
+public class Item58 {
+
+    static Collection<Suit> suits = List.of(Suit.values());
+    static Collection<Rank> ranks = List.of(Rank.values());
+
+    public static void main(String[] args) {
+        List<Card> deck = new ArrayList<>();
+
+        // 5줄의 코드지만 썩은내가 진동한다
+        for (Iterator<Suit> i = suits.iterator(); i.hasNext(); ) {
+            Suit suit = i.next();
+            for (Iterator<Rank> j = ranks.iterator(); j.hasNext(); ) {
+                deck.add(new Card(suit, j.next()));
+            }
+        }
+
+        // 으음 굿스멜
+        for (Suit suit : suits) {
+            for (Rank rank : ranks) {
+                deck.add(new Card(suit, rank));
+            }
+        }
+    }
+
+    enum Suit {
+        CLUB, DIAMOND, HEART, SPADE
+    }
+
+    enum Rank {
+        ACE, DEUCE, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING
+    }
+
+    static class Card {
+
+        private Suit suit;
+        private Rank rank;
+
+        public Card(Suit suit, Rank rank) {
+            this.suit = suit;
+            this.rank = rank;
+        }
+    }
+}
+
+/*
+for 문을 쓰는 목적이 무엇인가
+for -> index 를 이용해 값을 늘려가면서 일정한 값까지만 한정해두고 값을 더한다던지, 뺀다던지 등 지지고 볶는 연산이 있을 수 있고
+for-each -> Collection 넣어서 그 안의 요소들로 쌈싸먹는 연산이 있을 수 있다
+
+그럼 for-each 안 쓰고 for 쓴 놈들 대가리 깨야할까??, 놉 그건 아님
+1. destructive filtering, 순회하면서 요소 삭제해야할 때 iterator.remove() 쓰거나 Collection.removeIf() 써야함
+2. transforming, 순회하면서 값을 변경해야 할 때
+3. parallel iteration, 여러 컬렉션을 병렬로 순회해야 할 때 ?!
+
+병렬 반복이란 뭘까??
+ */
