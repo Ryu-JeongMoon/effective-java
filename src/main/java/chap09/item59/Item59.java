ea032d5ede64c2273ff7fbc48f65999ef6dfadb8
jm <rjm9303@gmail.com>
2022-01-01 22:57:11 +0900

    Item 59, 60

---
+++ src/main/java/chap09/item59/Item59.java
@@ -0,0 +1,46 @@
+package chap09.item59;
+
+import java.util.Random;
+import java.util.SplittableRandom;
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * 라이브러리를 익히고 사용하라
+ */
+public class Item59 {
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        random.nextInt(50);
+
+        ThreadLocalRandom current = ThreadLocalRandom.current();
+        current.nextInt(50);
+
+        SplittableRandom splittableRandom = new SplittableRandom();
+        splittableRandom.nextInt(50);
+    }
+}
+
+/*
+Random -> ThreadLocalRandom 으로 대체하면 성능 향상과 고품질의 난수 생성
+SplittableRandom -> Fork-Join pool / Parallel Stream 에서 사용한다면
+
+low-level 의 코드를 알아두면 좋지만 목적이 무엇인지에 따라 효율이 달라진다
+평범한 웹 백엔드를 다룰 때 SplittableRandom 의 사용법 외에 사용 원리까지 빠삭하게 알아야 할 필요가 있을까?
+core-module 관련 개발을 하는데 동작 방식을 싸그리 무시하고 사용법만 안다고 개발이 될까?
+
+java.lang, java.util, java.io, java.util.concurrent 와 같이 핵심 라이브러리는 알아두면 도움이 된다
+java 의 기초가 되는 lang
+성능 좋고 사용하기 쉽게 만들어진 util
+결국은 알아둬야 할 low-level 의 io
+앞으로 더 중요해질 동시성 관련 concurrent
+
+특정 기능이 필요하다고 무작정 구현하기 보다 표준 라이브러리에 그러한 기능이 있는지 먼저 살펴보도록 하자
+표준이란 의미에 맞게 극한의 최적화를 이루어 뒀을 것이고 수많은 이들의 사용 경험으로 검증이 되었을 것이다
+이래도 없다?! 그럼 만들자 or third-party library 이용하자
+
+동작 원리는 너무나 중요하다
+그러나 단계별로 알아야 하는, 알 수 있는 지식들은 차이가 있다
+자바 개허접인데 동시성 프로그래밍의 원리부터 구현까지 싸그리 알 수는 없는 법
+큰 그림을 먼저 파악한 후 정말 필요할 때 세부사항까지 공부해나가면 된다
+ */
---
+++ src/main/java/chap09/item60/Item60.java
@@ -0,0 +1,55 @@
+package chap09.item60;
+
+import java.math.BigDecimal;
+
+/**
+ * 정확한 답이 필요하다면 float, double 은 피하라
+ */
+public class Item60 {
+
+    public static void main(String[] args) {
+        // 0.6100000000000001
+        System.out.println(1.03 - 0.42);
+
+        calculateRemains();
+
+        calculateRemainsByInt();
+    }
+
+    static void calculateRemains() {
+        final BigDecimal TEN_CENTS = new BigDecimal(".10");
+
+        int itemsBought = 0;
+        BigDecimal funds = new BigDecimal("1.00");
+        for (BigDecimal price = TEN_CENTS; funds.compareTo(price) >= 0; price = price.add(TEN_CENTS)) {
+            funds = funds.subtract(price);
+            itemsBought++;
+        }
+        System.out.println(itemsBought + "개 구입");
+        System.out.println("잔돈 : " + funds);
+    }
+
+    // 소수점 없이 달러 -> 센트로 올리고 int 사용해 계산
+    static void calculateRemainsByInt() {
+        int itemsBought = 0;
+        int funds = 100;
+        for (int price = 10; funds >= price; price+=10) {
+            funds -= price;
+            itemsBought++;
+        }
+        System.out.println(itemsBought + "개 구입");
+        System.out.println("잔돈 : " + funds);
+    }
+}
+
+/*
+float / double 은 과학 & 공학 계산용으로 설계되었다
+정확성 보다는 근사치에 중점을 둔 것
+
+BigDecimal 은 느리고 사용법이 일반 +, - 연산과 달라 불편하다
+But 소수점 관리를 시스템에 맡겨버리고, 성능 저하에도 신경 쓰지 않는다면 사용하자
+
+int -> 21억 까지 사용 가능
+long -> 9223372036854775807 까지 사용 가능
+그 외에는 BigDecimal 사용 해야함
+ */
