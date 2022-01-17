a5a3335de01dd8bda001839f74a17850ce0d3bc3
jm <rjm9303@gmail.com>
2022-01-18 01:41:14 +0900

    [복습] Item 1, 2

--- src/main/java/chap02/item01/Item01.java
+++ src/main/java/chap02/item01/Item01.java
@@ -4,31 +4,49 @@ import static chap02.item01.Item01.Card.JACK;
 import static chap02.item01.Item01.Card.KING;
 import static chap02.item01.Item01.Card.QUEEN;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.math.BigInteger;
-import java.sql.Date;
+import java.nio.file.FileStore;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.SQLException;
 import java.time.Instant;
+import java.util.Date;
 import java.util.EnumSet;
+import java.util.List;
 
 /**
  * 생성자 대신 정적 팩터리 메서드를 고려하라
  */
 public class Item01 {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException, SQLException {
     Instant instant = Instant.now();
-    Date.from(instant);
+    Date date = Date.from(instant);
+    System.out.println("date = " + date);
 
-    EnumSet.of(JACK, QUEEN, KING);
+    EnumSet<Card> jack = EnumSet.of(JACK, QUEEN, KING);
+    System.out.println("jack = " + jack);
 
-    BigInteger.valueOf(Integer.MAX_VALUE);
+    BigInteger bigInteger = BigInteger.valueOf(Integer.MAX_VALUE);
+    System.out.println("bigInteger = " + bigInteger);
 
-    StackWalker.getInstance();
+    List<Object> of = List.of("YAHO");
+    System.out.println(of.get(0));
+    System.out.println("of.getClass() = " + of.getClass().getName());
+
+    StackWalker instance = StackWalker.getInstance();
+    System.out.println("instance = " + instance);
 
     // getType -> getInstance 와 같은데 Files 생성이 아니라 FileStore 생성인 것처럼 다른 클래스 생성할 때 사용
-    // Files.getFileStore(Path.of("어쩌구 저쩌구"));
+    FileStore blahBlah = Files.getFileStore(Paths.get("어쩌구저쩌구"));
+    long blockSize = blahBlah.getBlockSize();
+    System.out.println("blockSize = " + blockSize);
 
     // newType -> newInstance 와 같음, 다른 클래스 생성할 때 사용
-    // BufferedReader br = Files.newBufferedReader(path);
+    BufferedReader br = Files.newBufferedReader(Paths.get("어쩌구저쩌구"));
+    System.out.println("br.readLine() = " + br.readLine());
 
     // type -> getType, newType 간략 버전
     // List<Complaint> litany = Collections.list(legacyLitany);
@@ -40,23 +58,48 @@ public class Item01 {
 }
 
 /*
+Java8 전에는 인터페이스에 static method 있을 수 없었다
+Collection (Interface) -> Collections (Util Class) 처럼 인터페이스에 s 를 붙여 유틸 클래스를 만드는 것이 관례였다
+유틸 클래스에 static factory method 를 두어 여기서 Collections.emptyList() 같은 메서드 만들어두었다
+Java8 이후로는 인터페이스를 이용해 반환하면 되므로 의미가 많이 사라졌으나,
+Java9부터 인터페이스에 private static field / member class 를 둘 수 있어서 자바8을 사용하는 경우 유틸 클래스 사용할 필요가 있다
+
+기본적으로 클라이언트는 클래스의 인스턴스를 얻기 위해 new ClassName() 방식을 사용한다
+생성자와는 별도로 static factory method 를 이용하는 방법도 있다
+그에 따른 장/단점은 아래와 같다
+
 장점
 1. instance 생성 메서드의 이름을 가질 수 있다
-getInstance() 등과 같이 일반적인 네이밍도 가능하고 구체적으로도 가능
+일반 생성자의 경우 객체의 특성을 제대로 설명하지 못 한다
+또한 같은 시그니처로는 생성자를 하나만 만들 수 있다, 이를 생성자 오버로딩을 통해 극복할 수도 있지만
+생성자가 여러개 존재할 때, 어떤 것이 필요할지 어떻게 알 수 있을까?
+API 작성자는 의도를 드러내어 만들었다고 생각하겠지만 사용하는 입장에서는 주석을 참고하거나 사용되는 Context 알고 클래스를 까봐야 알 수 있다
+정적 팩토리 메서드는 getInstance() 등과 같이 일반적인 네이밍도 가능하고 구체적으로도 가능
+효과적인 네이밍을 통해 클래스를 까보지 않아도 어떤 상황에 쓰일 수 있는지 드러낼 수 있다
+
 2. 호출될 때마다 인스턴스를 생성할 필요가 없다
-singleton ??
+singleton
+
 3. 반환 타입의 하위 타입 객체를 반환할 수 있다
-List.of() 같은 메서드를 이용할 때 ArrayList 로 반환해버린다는 뜻?!
+List.of() 로 생성 시 java.util.ImmutableCollections$ListN 형태의 리스트 생성됨, 요놈은 읽기 전용
+
 4. 입력 매개변수에 따라 다른 클래스의 객체를 반환할 수 있다
 EnumSet 의 경우 매개변수가 가진 요소의 개수에 따라 64개를 기준으로 이하일 때 RegularEnumSet, 초과일 때 JumboEnumSet 반환
+다음 릴리즈때 구현체를 바꾸더라도 클라이언트는 상관이 없다?!
+이전 버전에 맞게 특화된 코드들이 있을 수 있으므로 상관이 없는 건 아닐듯, 단지 구현체를 바꾸더라도 터지지 않고 잘 돌아간다 정도로 해석
+
 5. 정적 팩터리 메서드를 작성하는 시점에는 반환될 객체의 클래스가 존재하지 않아도 된다
-오잉 이게 뭔 소리임
+독립적인 개발을 진행할 때 Interface 만들어두고 해당 인터페이스 구현하는 임시 객체를 사용하는 것처럼 활용 가능
+지금 당장 없어도 애플리케이션 통합할 때 실제 클래스 존재하면 됨
 
 단점
-1. 상속을 위해서는 public or protected 생성자 필요, 정적 팩터리 메서드만 제공하기 위해서는 상속 불가
+1. 상속을 위해서는 public or protected 생성자 필요, 정적 팩터리 메서드만 제공할 때는 상속 불가
 이 제약은 상속보다 합성을 사용하도록 유도하기 때문에 오히려 좋아
+
 2. 정적 팩터리 메서드는 프로그래머가 찾기 어렵다??
-생성자는 한 눈에 바로 들어와서 파악이 쉬운데, 팩터리 메서드는 안 그러니까? API 문서를 잘 작성하고, 관례에 따라 네이밍하도록 한다
+생성자는 한 눈에 바로 들어와서 아 여기서 생성하는고나 하고 파악이 쉬운데,
+팩터리 메서드 자체가 어디서 사용되는지도 찾아야되고 어떤 인스턴스 반환하는지 클래스 또 까봐야 안다
+API 문서를 잘 작성하고, 관례에 따라 네이밍하도록 한다
 
 각각의 쓰임새가 있으니 필요에 따라 선택하도록 하거라
  */
--- src/main/java/chap02/item02/Item02.java
+++ src/main/java/chap02/item02/Item02.java
@@ -10,7 +10,8 @@ public class Item02 {
 
   public static void main(String[] args) {
     NutritionFacts nutritionFacts = NutritionFacts.builder()
-      .calories(500).build();
+      .calories(500)
+      .build();
 
     System.out.println(nutritionFacts);
   }
@@ -29,11 +30,44 @@ public class Item02 {
 }
 
 /*
-모든 필드가 필수가 아닐 때, 생성자를 이용하면 혼란스러워짐 요럴 때 Builder 써라!
-점층적 생성자 패턴도 사용 가능하지만 일일이 다 생성하기 넘 힘드렁 변경에 유연하게 대응하기 위해서는 Builder 써야 함
-lombok 에서 제공해주니까 편하게 쓰장
+인스턴스 생성할 때 받아야 하는 매개변수가 무지막지 하다면 어떻게 해야할까?
+1. telescoping constructor pattern
+2. java beans pattern
+3. builder pattern
 
-Java Beans Pattern 이라는 것도 존재하는데 기본 생성자를 하나 두고 setter 로 필드 값을 지정해준다
+점층적 생성자 패턴은 받아야 하는 생성자 오버로딩을 통해 매개변수 수를 늘려가는 것
+매개변수 개수에 따라 계속 늘어나야 하므로 유지보수까지 생각한다면 이건 지옥
+
+Java Beans Pattern 은 기본 생성자를 하나 두고 setter 로 필드 값을 지정해준다
 하나의 instance 만드려면 setter 난무 해야하고 필수 값을 빠뜨리고 생성할 수도 있기 때문에 일관성이 보장되지 않는다
 setter 를 열어뒀기 때문에 불변 객체로 만들 수 없다는 단점도 있다
+
+모든 필드가 필수가 아닐 때, 생성자를 이용하면 혼란스러워짐 요럴 때 Builder 써라!
+점층적 생성자 패턴도 사용 가능하지만 일일이 다 생성하기 넘 힘드렁 변경에 유연하게 대응하기 위해서는 Builder 써야 함
+lombok 에서 제공해주니까 편하게 쓰장, 롬복이 대략 요런 놈을 만들어 주는 것
+
+public static class Builder {
+    private final int servingSize;
+    private final int servings;
+    private int calories;
+    private int fat;
+    private int sodium;
+    private int carbohydrate;
+
+    public Builder(int servingSize, int servings) {
+      this.servingSize = servingSize;
+      this.servings = servings;
+    }
+
+    final 아닌 것들의 만들어지는 형태
+    public Builder calories(int calories) {
+      this.calories = calories;
+      return this;
+    }
+}
+
+불변과 불변식
+immutability 는 어떠한 변경도 허용하지 않는다
+invariant 는 프로그램 실행 동안 변경은 허용되나 특정 조건 하에서만 가능하다
+ex) List 의 size 는 0 미만일 수 없다, Period 의 start 는 end 보다 항상 앞서야 한다
  */
