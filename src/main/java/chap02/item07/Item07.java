8db8e6f3bc6b7ecff80fb27be784d88a0ea8dbbe
jm <rjm9303@gmail.com>
2022-02-12 00:22:02 +0900

    [복습] Item 7 잘못 이해한 내용 수정

--- src/main/java/chap02/item07/Item07.java
+++ src/main/java/chap02/item07/Item07.java
@@ -1,12 +1,24 @@
 package chap02.item07;
 
+import java.util.Stack;
+
 /**
  * 다 쓴 객체 참조를 해제하라
  */
 public class Item07 {
 
   public static void main(String[] args) {
+    Stack<Integer> stack = new Stack<>();
+    stack.push(1);
+    stack.push(2);
 
+    System.out.println(stack.pop());
+
+    chap02.item07.Stack customStack = new chap02.item07.Stack();
+    customStack.push(1);
+    customStack.push(2);
+
+    System.out.println(customStack.pop());
   }
 }
 
--- src/main/java/chap02/item07/Stack.java
+++ src/main/java/chap02/item07/Stack.java
@@ -22,7 +22,7 @@ public class Stack {
     if (size == 0) {
       throw new EmptyStackException();
     }
-    Object result = elements[size--];
+    Object result = elements[--size];
 
     // 다시 안 쓸 요놈 메모리 할당을 해제해줘야 한다
     elements[size] = null;
@@ -39,9 +39,9 @@ public class Stack {
 /*
 책에서는 elements[--size] 로 되어있는디
 일반적으로 pop 메서드는 마지막 요소를 삭제 후 반환해줘야 하는데 그럼 elements[size--] 로 해야하지 않나?
+오잉 아니네, 인덱스로 반환하고 size 줄어야 하니까 -- 먼저 때려야 하는구만
 
 참조 해제를 안 하는 경우 최악의 상황에서는 해제되어야 할 객체가 참조하는 많은 객체들이 회수되지 않을 수 있다
-
 프로그램이 거대해질 수록 메모리 누수를 정확히 파악하기 어렵기 때문에
 메모리를 직접 다루는 클래스를 작성할 경우 테스트에 더 주의해야 하고 성능 테스트도 수행해야 한다
  */
