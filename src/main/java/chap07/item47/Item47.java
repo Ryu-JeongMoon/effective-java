cb104f0fe935154bd609533ab245b50ce0392ac8
jm <rjm9303@gmail.com>
2022-02-12 01:51:15 +0900

    [복습] Item 47, 48

--- src/main/java/chap07/item47/Item47.java
+++ src/main/java/chap07/item47/Item47.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 /**
  * 반환 타입으로는 스트림보다 컬렉션이 낫다
@@ -16,12 +17,12 @@ public class Item47 {
 //            System.out.println("yaho");
 //        }
 
-//        사용할 순 있지만 (Iterable<ProcessHandle>) 추잡하다
+    // 사용할 순 있지만 (Iterable<ProcessHandle>) 추잡하다
     for (ProcessHandle ph : (Iterable<ProcessHandle>) ProcessHandle.allProcesses()::iterator) {
       System.out.println("ph = " + ph);
     }
 
-//        Adapter 를 사용하자
+    // Adapter 사용하자
     for (ProcessHandle processHandle : iterableOf(ProcessHandle.allProcesses())) {
       System.out.println("processHandle = " + processHandle);
     }
@@ -50,6 +51,24 @@ public class Item47 {
 
 왜 어려운가?
 스트림을 이용해 일련의 원소를 다룰 때 List, Map 등으로 고려하고 시작하지 않나?!
+Stream<Type> 방식으로도 반환이 가능한데 얘는 for-each 반복을 지원해주지 않는다
+Iterable 인터페이스가 정의한 대로 동작하지만 Iterable 을 확장하지 않았기 때문이다
+
+문제 해결 방법에는 여러 가지가 있을 수 있다
+문제 해결 했다고 끝이 아니다, 어떤 방식이 왜 좋은지 알고 쓰자
+파일을 읽어들일 때 Scanner 이용한 방식과 Files.lines() 이용하는 방식이 있는데
+둘 중 Files.lines() 는 파일을 읽을 때 발생할 수 있는 문제를 해결한다는 점으로 보아 얘가 우수한 것을 알 수 있다
+lines 의 상세구현으로 들어가 createFileChannelLinesStream 얘를 살펴보면 FileChannelLinesSpliterator 로 읽어들이고
+StreamSupport 를 이용해 Stream 형태로 반환해준다 try-catch 로 예외도 다 잡고 처리해놨다
+
+또한 널리 알려진 것이나 만들어진 형태가 없으면 직접 만들어 해결하는 방법도 있다
+iterableOf() 가 그 예시인데 단순히 내부에서 stream::iterator 를 조지는 것 뿐인데
+외부에서 사용할 때 더러운 (Iterable<ProcessHandle>) 형변환을 하지 않게끔 만드는 유용한 메서드다
+이를 변환 시켜준다는 의미의 Adapter 라 하는데 재사용성을 높이기 위해 디자인 패턴을 잘 알아야 하겠다
+
+Stream <-> Iterable 상호 호환이 완벽하지 않기 때문에 Collection 을 사용하는 편이 낫다
+Collection 은 Iterable 구현하고 있고 stream() 메서드로 이미 어댑터가 구현되어 있기 때문이다
+Stream 을 반환하게 되면 직접 어댑터를 작성해서 해결할 수 있는데 귀찮슴둥
 
 반환할 시퀀스의 크기에 따라 메모리에 올려도 될 정도라면 ArrayList, HashSet 등의 구현체로 반환해도 되지만
 지나치게 큰 경우 AbstractList 를 이용해야 한다
--- src/main/java/chap07/item48/Item48.java
+++ src/main/java/chap07/item48/Item48.java
@@ -18,14 +18,15 @@ public class Item48 {
 //            .forEach(System.out::println);
 
     long pow = (long) (Math.pow(10, 8));
-    long start = System.currentTimeMillis();
+    long start = System.nanoTime();
     System.out.println(pi(pow));
-    long end = System.currentTimeMillis();
-    System.out.println("end-start = " + (end - start) + "ms");
+    long end = System.nanoTime();
+    System.out.println("end-start = " + (end - start) + "ns");
   }
 
   static long pi(long n) {
     return LongStream.rangeClosed(2, n)
+      .parallel()
       .mapToObj(BigInteger::valueOf)
       .filter(i -> i.isProbablePrime(50))
       .count();
@@ -35,11 +36,26 @@ public class Item48 {
 /*
 데이터 소스가 Stream.iterate 이거나 중간 연산으로 limit 을 쓰는 경우에 파이프라인 병렬화는 무력화된다
 스트림의 소스가 ArrayList, HashMap, HashSet 등의 구현체의 인스턴스일 때, 배열 & int / long 범위일 때 효과가 가장 좋다
-1. 데이터를 원하는 크기로 잘라 스레드에 분배하기 좋기 때문
+1. 데이터를 원하는 크기로 잘라 스레드에 분배하기 좋기 때문 (데이터를 잘라주는 작업은 Spliterator 가 해준다)
 2. 이들은 참조 지역성이 좋다, 즉 저장된 위치들에 연속성이 있어 데이터를 가져올 때 최대한 유사한 애들까지 같이 가져온다
+
+참조 지역성이 나쁘면 어떻게 되는가?
+Disk -> Memory -> Cache 계속 퍼올려야 하고 Disk 부터 퍼올리는 작업은 개같이 느리기 때문에 CPU 는 노는 중
+이 때문에 인덱스를 가지고 다닥다닥 붙어있는 배열 쓰는게 성능 상 제일 좋다
+
+스트림 사용할 때 종단 연산의 중요성이 큰데 일단 이 놈이 있어야 연산이 끝마쳐지고
+종단에서 어떤 연산을 하느냐에 따라 성능이 좌우된다
+중간 연산 야무지게 해놓고 종단에서 순차적 연산을 걸면 병렬화 무력화
+종단 연산에서 연산 효율이 가장 좋은 것은 결과를 줄여주는 reduce 일족 -> 그래서 빅데이터 처리 시 Map-Reduce 작업이라 하는고만
+anyMatch, allMatch, noneMatch 처럼 early return 가능한 놈들도 굳임
 
 계산이 올바르게 수행되는지, 성능이 정말 향상됐는지 파악하고 스트림 병렬화를 이용해야 한다
 동시성은 어려운 문제고 성능 지표를 직접 따져보며 사용해야 하므로 잘 모르겠으면 일단 쓰지 말고
 충분한 테스트를 거친 후에 사용하도록 한다
 일반적으로 CPU 집중적인 연산이 많은 상황에 도입을 고려해볼만 하다
+
+스트림 원소 수 x 원소 당 수행되는 코드 줄 => 수십만줄 이상일 때 성능 향상
+데이터를 자르고 연산 처리하고 데이터를 다시 모을 때 다 오버헤드 발생하니께
+스트림 병렬화를 자바 애플리케이션에서 쓸 일은 거의 없겠구먼
+이런 작업은 빅데이터 처리에 특화된 툴을 이용하겄다
  */
